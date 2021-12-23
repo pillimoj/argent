ac3708b456117deb7416a2521b884b2b84af4510
Eric Grimsborn <eric.o.berglund@gmail.com>
2021-12-23 19:43:23 +0100

    update deps (#5)

--- src/util/Json.kt
+++ src/util/Json.kt
@@ -4,11 +4,20 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.SingletonSupport
 
 val defaultJackson: ObjectMapper.() -> Unit = {
     registerModules(
-        KotlinModule(),
+        KotlinModule.Builder()
+            .withReflectionCacheSize(512)
+            .disable(KotlinFeature.NullToEmptyCollection)
+            .disable(KotlinFeature.NullToEmptyMap)
+            .disable(KotlinFeature.NullIsSameAsDefault)
+            .disable(KotlinFeature.SingletonSupport)
+            .disable(KotlinFeature.StrictNullChecks)
+            .build(),
         JavaTimeModule()
     )
     disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
